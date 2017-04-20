be3963b205d1bf621a150bef03bf52f392e87c09
Pral2a <guillem@pral2a.com>
2017-04-20 15:52:30 +0200

    URL hash for sorting content

--- js/sort.js
+++ js/sort.js
@@ -1,15 +1,29 @@
-var $boxs = $("#parent > .box");
-var $btns = $(".sort").on("click", function() {
-  
-  var active = 
-    $btns.removeClass("active")
-      .filter(this)
-      .addClass("active")
-      .data("filter");
-  
-  $boxs
-    .hide()
-    .filter( "." + active )
-    .fadeIn(450);
+// On page ready
+$(document).ready(function() {
+	// On the URL hash changed
+	$(window).on('hashchange', function() {
+		
+		var $boxs = $("#parent > .box");
 
+		// Gets the hash name on the URL and removes 
+		// the # from the beginning
+		var activeClass = location.hash.substring(1);
+
+		console.log();
+
+		if (activeClass && $boxs.hasClass(activeClass)) {
+		// If the hash the name of one of the classes
+		// use it to filter the category
+			$boxs
+				.hide()
+				.filter("." + activeClass)
+				.fadeIn(450);
+		} else {
+		// If the hash doesn't exist remove any filters
+			$boxs
+				.show()
+				.fadeIn(450);
+		}
+
+	}).trigger('hashchange');
 });
